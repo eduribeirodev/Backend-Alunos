56719e25daa1800da3a9514061c2edaa8e8c7ed2
eduribeirodev <eduardoribeiro0912@gmail.com>
2026-05-28 00:12:02 -0300

    refactor: remove repository duplicado de trabalho em grupo

--- src/main/java/com/agrupa/tat_3ds/repository/TrabalhoEmGrupoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.agrupa.tat_3ds.repository;
-
-import com.agrupa.tat_3ds.models.TrabalhoEmGrupo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TrabalhoEmGrupoRepository extends JpaRepository<TrabalhoEmGrupo, Integer> {
-}
