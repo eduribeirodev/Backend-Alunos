97f0f6306bf13942db7e95ebb423d2a0fdb15209
eduribeirodev <eduardoribeiro0912@gmail.com>
2026-05-28 00:15:04 -0300

    fix: corrige nome da tabela trabalho_em_grupo

--- src/main/java/com/agrupa/tat_3ds/models/TrabalhoEmGrupo.java
+++ src/main/java/com/agrupa/tat_3ds/models/TrabalhoEmGrupo.java
@@ -1,11 +1,16 @@
 package com.agrupa.tat_3ds.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Data;
 
 @Getter
 @Setter
@@ -13,7 +18,7 @@ import lombok.Data;
 @AllArgsConstructor
 @Entity
 @Data
-@Table(name = "trabalho_em_groupo")
+@Table(name = "trabalho_em_grupo")
 public class TrabalhoEmGrupo {
 
     @Id
